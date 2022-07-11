d91949504bdfb7efc3052ce569d0ac06d0e9e451
Yevhen Danilov <danilov.evgen@gmail.com>
2022-07-11 22:51:10 +0300

    diplomas upd

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,14 @@
-$(".diplomas-slider").slick({
+$(".certificates-slider").slick({
   infinite: true,
   slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+});
+
+$(".projects-slider").slick({
+  infinite: true,
+  slidesToShow: 2,
   slidesToScroll: 1,
   arrows: true,
   dots: true,
